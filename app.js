f0fefb3338866aeef21f6960a7ce8f412007cb62
Abhiraj Maurya <abhirajmaurya1456@gmail.com>
2026-01-09 14:48:42 +0530

    Corner cases handled

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let input = document.querySelector("input");
 let ul = document.querySelector("ul");
 let btn = document.querySelector("#add");
 btn.addEventListener("click", function () {
+    if(input.value.trim()){
     let li = document.createElement("li");
     let del = document.createElement("button");
     del.classList.add("delete");
@@ -10,6 +11,10 @@ btn.addEventListener("click", function () {
     li.append(del);
     ul.appendChild(li);
     input.value = "";
+}
+else{
+    alert("Please enter something in task to add!")
+}
 });
 ul.addEventListener("click", function (e) {
     if (e.target.nodeName == "BUTTON") {
